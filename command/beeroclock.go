4fa436fd05c49693a7679b0af0e394d71d5ed3f7
Caio Filipini <caiofilipini@gmail.com>
2014-08-22 17:08:21 +0200

    Improve beer oclock with queries for time remaining.

--- command/beeroclock.go
+++ command/beeroclock.go
@@ -2,19 +2,42 @@ package command
 
 import (
 	"fmt"
+	"regexp"
 	"time"
 )
 
 const (
-	Beer = "\U0001f37a"
+	Beer         = "\U0001f37a"
+	Clock        = "\U000023f0"
+	StartingHour = 18
 )
 
+var queryRegexp *regexp.Regexp
+
+func init() {
+	queryRegexp = regexp.MustCompile(`(?i)time|long|til|left|remaining|eta`)
+}
+
 func BeerOclock(query string) []string {
-	hour := time.Now().Hour()
+	now := time.Now()
+	hour := now.Hour()
 
 	var result string
-	if hour >= 18 {
-		result = fmt.Sprintf("YES! %s", Beer)
+
+	if queryRegexp.MatchString(query) {
+		hourDiff := StartingHour - hour - 1
+		minuteDiff := 60 - now.Minute()
+		secondDiff := 60 - now.Second()
+
+		result = fmt.Sprintf(
+			"%s%s in %d hour(s), %d minute(s) and %d second(s)",
+			Beer,
+			Clock,
+			hourDiff,
+			minuteDiff,
+			secondDiff)
+	} else if hour >= StartingHour {
+		result = fmt.Sprintf("YES! %s%s%s", Beer, Beer, Beer)
 	} else {
 		result = "Not yet. :("
 	}
